acd48fe2a854d5df81a1a305961b391d5356c752
noki <novak.kirill@gmail.com>
2021-07-02 14:58:51 +0300

    init

---
+++ src/components/Result.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css"
+
+const Result = ()=>{
+    return(
+        <div className="table_container">
+        <p>Poka</p>
+      </div>
+    )
+};
+
+export default Result;
---
+++ src/components/SearchLogin.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+const SearchLogin = () => {
+
+    
+  return (
+    <div className="search_container">
+      <p>Privet!</p>
+    </div>
+  );
+};
+
+export default SearchLogin;
